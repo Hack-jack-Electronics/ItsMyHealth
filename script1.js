45ffbf4466ecd46eeb51e948c87a58477704e2b2
Tanishk Singhal <147161837+Hack-jack-Electronics@users.noreply.github.com>
2024-05-15 16:52:42 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,5 +0,0 @@
-function openChatbot() {
-    // Implement your open chatbot function here
-    var chatbotContainer = document.getElementById("chatbotContainer");
-    chatbotContainer.style.display = "block";
-}
